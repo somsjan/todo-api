946c6e97d5e97b0d1c784d4a3f332e2cd728815e
Jan v/d Molen <janvdm89@hotmail.com>
2017-05-11 19:22:24 +0200

    Added POST todos route

--- server/server.js
+++ server/server.js
@@ -1,31 +1,28 @@
-const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/ToDoApp');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    },
-    completedAt: {
-        type: Number,
-        default: null
-    }
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (err) => {
+        res.status(400).send(err);
+    });
 });
 
-var newTodo = new Todo({
-    text: ' Kadubges   '
-});
 
-newTodo.save().then((doc) => {
-    console.log('Saved todo ', doc);
-}, (e) => {
-    console.log('Unable to save', e.message)
+
+app.listen(3000, () => {
+    console.log('Starting on port 3000');
 });
