6b93cf4a514adfc25f6d4e345c503c70ef080469
Jan v/d Molen <janvdm89@hotmail.com>
2017-05-14 15:12:45 +0200

    fixed DB connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds141401.mlab.com:41401/todo-api' ||
-                'mongodb://localhost:27017/ToDoApp');
+// mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds141401.mlab.com:41401/todo-api' ||
+//                 'mongodb://localhost:27017/ToDoApp');
+//
+
+    var localDB = 'mongodb://localhost:27017/ToDoApp';
+    var mLab = 'mongodb://admin:123@ds141401.mlab.com:41401/todo-api';
+
+mongoose.connect(mLab || localDB);
 
 module.exports = {
     mongoose
