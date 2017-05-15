06fdbe16663b7938b7bb15ecae730239999859ed
Jan v/d Molen <janvdm89@hotmail.com>
2017-05-15 20:39:27 +0200

    Added test UPDATE on route /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -11,6 +11,8 @@ const todos = [{
 }, {
     _id: new ObjectId(),
     text: 'Second test todo',
+    completed: true,
+    completedAt: 333
 }];
 
 beforeEach((done) => {
@@ -145,4 +147,45 @@ describe('DELETE /todos/:id', () =>{
         .expect(404)
         .end(done);
     });
+});
+
+
+describe('PATCH /todos/:id', () => {
+    it('should update the todo', (done) =>{
+        var hexId = todos[0]._id.toHexString();
+        var text = "Super nieuwe text";
+
+        request(app)
+        .patch(`/todos/${hexId}`)
+        .send({
+            completed: true,
+            text
+        })
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo.text).toBe(text);
+            expect(res.body.todo.completed).toBe(true);
+            expect(res.body.todo.completedAt).toBeA('number');
+        }).end(done);
+    });
+
+    it('should clear completedAt when todo is not completed', (done) =>{
+        var hexId = todos[1]._id.toHexString();
+        var text = "De allernieuwste text";
+
+        request(app)
+        .patch(`/todos/${hexId}`)
+        .send({
+            completed: false,
+            text
+        })
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo.text).toBe(text);
+            expect(res.body.todo.completed).toBe(false);
+            expect(res.body.todo.completedAt).toNotExist();
+        }).end(done);
+    });
+
+
 });
