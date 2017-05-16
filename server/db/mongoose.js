911bf6edc2152ae2641c19da4d4795e337e96c71
Jan v/d Molen <janvdm89@hotmail.com>
2017-05-16 14:46:28 +0200

    Setup reparate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,20 +1,12 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds141401.mlab.com:41401/todo-api' ||
-//                 'mongodb://localhost:27017/ToDoApp');
-//
 
-// var localDB = 'mongodb://localhost:27017/ToDoApp';
-// var mLab = 'mongodb://admin:123@ds141401.mlab.com:41401/todo-api';
-// mongoose.connect(localDB || mLab);
-
-var url = process.env.MONGODB_URI || 'mongodb://localhost:27017/ToDoApp';
+var url = process.env.MONGODB_URI;
+// var url = process.env.MONGODB_URI || 'mongodb://localhost:27017/ToDoApp';
 mongoose.connect(url);
 
 
 module.exports = {
     mongoose
 };
-
-//heroku config:set MONGODB_URI=mongodb://admin:123@ds141401.mlab.com:41401/todo-api
