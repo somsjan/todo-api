b59429c60fc3a8520327dcf490bc59e4662a7732
Jan v/d Molen <janvdm89@hotmail.com>
2017-05-17 15:34:27 +0200

    Add POST /users route + fixed path to dbs

--- server/config/config.js
+++ server/config/config.js
@@ -2,8 +2,16 @@ var env = process.env.NODE_ENV || 'development';
 
 if (env === 'development'){
     process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/ToDoApp';
+    if (process.env.PORT == 3000) {
+        process.env.MONGODB_URI = 'mongodb://localhost:27017/ToDoApp';
+        console.log('Using local DB');
+    } else {
+        process.env.MONGODB_URI = 'mongodb://admin:123@ds141401.mlab.com:41401/todo-api';
+        console.log('Using mLab DB');
+    }
+
 } else if(env === 'test'){
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/ToDoAppTest';
+    console.log('Using local TEST db');
 }
--- server/server.js
+++ server/server.js
@@ -11,11 +11,9 @@ var {User} = require('./models/user');
 
 var app = express();
 const port = process.env.PORT || 3000;
-if(process.env.PORT){
-    process.env.MONGODB_URI = 'mongodb://admin:123@ds141401.mlab.com:41401/todo-api';
-}
 
 app.use(bodyParser.json());
+
 
 app.post('/todos', (req, res) => {
     var todo = new Todo({
@@ -100,6 +98,19 @@ app.patch('/todos/:id', (req, res) =>{
         res.status(400).send();
     });
 
+});
+
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((err) => {
+        res.status(400).send(err);
+    });
 });
 
 app.listen(port, () => {
