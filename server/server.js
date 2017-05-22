b2cad7caba2008e4ffa4bce260f7c27838e637d0
Jan v/d Molen <janvdm89@hotmail.com>
2017-05-22 19:57:00 +0200

    Added DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -135,6 +135,15 @@ app.post('/users/login', (req, res) => {
 
 });
 
+//logout
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () => {
+        res.status(400).send();
+    })
+});
+
 app.listen(port, () => {
     console.log(`Starting up at port: ${port}`);
 });
