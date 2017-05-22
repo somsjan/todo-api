67e817cfb0a1bbe3a7df40e065419f19425b7a7d
Jan v/d Molen <janvdm89@hotmail.com>
2017-05-22 20:47:46 +0200

    Added DELETE /users/me/token test route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -305,3 +305,32 @@ describe('POST /users/login', () =>{
         })
     });
 });
+
+//delete request
+//set x-auth to equal to token
+//200
+//.end - find user that tokens array.length toBe 0
+describe('DELETE /users/me/token', () => {
+    it('should remove auth token on logout', (done) => {
+        // var token = users[0].tokens[0].token;
+        //console.log(token)
+
+        request(app)
+        .delete('/users/me/token')
+        // .send({'x-auth': token})
+        .set('x-auth', users[0].tokens[0].token)
+        .expect(200)
+        .end((err, res) => {
+            if (err){
+                return done(err)
+            }
+
+            User.findById(users[0]._id).then((user) => {
+                expect(user.tokens.length).toBe(0);
+                done();
+            }).catch((e) => {
+                return done(e)
+            });
+        });
+    });
+});
